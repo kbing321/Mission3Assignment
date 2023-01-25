a853d09f65fb1e97ef573d02b0b7c1d02bc1208a
nathanielbuck1 <nathanielbuck1@gmail.com>
2023-01-25 14:07:46 -0700

    Added stuff to supportingclass

--- Mission3Assignment/SupportingClass.cs
+++ Mission3Assignment/SupportingClass.cs
@@ -3,25 +3,110 @@ namespace Mission3Assignment
 {
     public class SupportingClass
     {
+        public void Winner(char[] arr)
+        {
+            //loop to determine winner
+            for (int i = 0; i < arr.Length; i++)
+            {
+                //option1
+                if ((arr[1] & arr[2] & arr[3]) == 'X')
+                {
+                    Console.WriteLine("Player 1 Wins!");
+                    //GamePlay = 1
+                }
+                else if ((arr[1] & arr[2] & arr[3]) == 'O')
+                {
+                    Console.WriteLine("Player 2 Wins!");
+                    //GamePlay = 1
+                }
+                //option2
+                else if ((arr[4] & arr[5] & arr[5]) == 'X')
+                {
+                    Console.WriteLine("Player 1 Wins!");
+                    //GamePlay = 1
+                }
+                else if ((arr[4] & arr[5] & arr[6]) == 'O')
+                {
+                    Console.WriteLine("Player 2 Wins!");
+                    //GamePlay = 1
+                }
+                //option3
+                else if ((arr[7] & arr[8] & arr[9]) == 'X')
+                {
+                    Console.WriteLine("Player 1 Wins!");
+                    //GamePlay = 1
+                }
+                else if ((arr[7] & arr[8] & arr[9]) == 'O')
+                {
+                    Console.WriteLine("Player 2 Wins!");
+                    //GamePlay = 1
+                }
+                //option4
+                else if ((arr[1] & arr[5] & arr[9]) == 'X')
+                {
+                    Console.WriteLine("Player 1 Wins!");
+                    //GamePlay = 1
+                }
+                else if ((arr[1] & arr[5] & arr[9]) == 'O')
+                {
+                    Console.WriteLine("Player 2 Wins!");
+                    //GamePlay = 1
+                }
+                //option5
+                else if ((arr[3] & arr[5] & arr[7]) == 'X')
+                {
+                    Console.WriteLine("Player 1 Wins!");
+                    //GamePlay = 1
+                }
+                else if ((arr[3] & arr[5] & arr[7]) == 'O')
+                {
+                    Console.WriteLine("Player 2 Wins!");
+                    //GamePlay = 1
+                }
+                //option6
+                else if ((arr[1] & arr[4] & arr[7]) == 'X')
+                {
+                    Console.WriteLine("Player 1 Wins!");
+                    //GamePlay = 1
+                }
+                else if ((arr[1] & arr[4] & arr[7]) == 'O')
+                {
+                    Console.WriteLine("Player 2 Wins!");
+                    //GamePlay = 1
+                }
+                //option7
+                else if ((arr[2] & arr[5] & arr[8]) == 'X')
+                {
+                    Console.WriteLine("Player 1 Wins!");
+                    //GamePlay = 1
+                }
+                else if ((arr[2] & arr[5] & arr[8]) == 'O')
+                {
+                    Console.WriteLine("Player 2 Wins!");
+                    //GamePlay = 1
+                }
+                //option8
+                else if ((arr[3] & arr[6] & arr[9]) == 'X')
+                {
+                    Console.WriteLine("Player 1 Wins!");
+                    //GamePlay = 1
+                }
+                else if ((arr[3] & arr[6] & arr[9]) == 'O')
+                {
+                    Console.WriteLine("Player 2 Wins!");
+                    //GamePlay = 1
+                }
+
+                else
+                {
+                    Console.WriteLine("Draw!");
+                    //GamePlay = -1
+                }
+            }
+        }
         public SupportingClass()
         {
-            int[] board = new int[] { 1, 2, 3, 4, 5, 6 };
 
-            int[] player1 = new int[] { };
-            int[] player2 = new int[] { };
-            int[] win1 = { 1, 2, 3 };
-            int[] win2 = { 4, 5, 6 };
-            int[] win3 = { 7, 8, 9 };
-            int[] win4 = { 1, 4, 7 };
-            int[] win5 = { 2, 5, 8 };
-            int[] win6 = { 3, 6, 9 };
-            int[] win7 = { 1, 5, 9 };
-            int[] win8 = { 3, 5, 7 };
-
-            for (int i = 0; i < player1.Length; i++)
-            {
-
-            }
         }
         public void Board(char[] arr)
         {
