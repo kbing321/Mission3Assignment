275daf6709f2086abf254d7747961c5c069d911d
Kohlton Bingham <kohltonhbingham@gmail.com>
2023-01-25 12:58:22 -0700

    Added supportclass file

---
+++ Mission3Assignment/SupportingClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Mission3Assignment
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+            int[] board = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            int[] player1 = new int[] { };
+            int[] player2 = new int[] { };
+            int[] win1 = { 1, 2, 3 };
+            int[] win2 = { 4, 5, 6 };
+            int[] win3 = { 7, 8, 9 };
+            int[] win4 = { 1, 4, 7 };
+            int[] win5 = { 2, 5, 8 };
+            int[] win6 = { 3, 6, 9 };
+            int[] win7 = { 1, 5, 9 };
+            int[] win8 = { 3, 5, 7 };
+
+            for (int i = 0; i < player1.Length; i++)
+            {
+
+            }
+        }
+    }
+}
