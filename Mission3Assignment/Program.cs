c893dccdbddcbf3fe9e4085ffd4d83aa371caf95
Kohlton Bingham <kohltonhbingham@gmail.com>
2023-01-25 13:49:47 -0700

    Added a lot of stuff

--- Mission3Assignment/Program.cs
+++ Mission3Assignment/Program.cs
@@ -7,6 +7,7 @@ namespace Mission3Assignment
         public static void Main(string[] args)
         {
             SupportingClass sc = new SupportingClass(); //declaration and instantiation
+            bool keepPlaying = true;
 
             Console.WriteLine("Welcome to this game of Tic Tac Toe!");
             Console.WriteLine("Please enter Player1's Name:");
@@ -14,50 +15,67 @@ namespace Mission3Assignment
             Console.WriteLine("Please enter Player2's Name:");
             string Player2 = Console.ReadLine();
 
-            char[] PlayerChoice = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+            char[] PlayerChoice = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
             // method to display the board
 
-            // maybe use a random number between 1 and 2 to choose which player goes first
-            int whoGoesFirst = 0;
-            Random randNum = new Random();
-            whoGoesFirst = randNum.Next(1, 2);
-
-
-            if (whoGoesFirst == 1)
-            {
-                string FirstPlayer = Player1;
-                string SecondPlayer = Player2;
-            }
-            else
-            {
-                string FirstPlayer = Player2;
-                string SecondPlayer = Player1;
-            }
+            
             // do a for loop 9 times for each player.
-            for (int i = 1; i < 10; i++)
+            while (keepPlaying == true)
             {
-                sc.Board(PlayerChoice);
-                if (i % 2 == 0)
+                for (int i = 1; i < 10; i++)
                 {
-                    // Ensure number is between 1 and 9?
-                    // Error message that won't allow me to add in FirstPlayer
-                    
-                    Console.WriteLine("Which number on the board would you like to choose?");
-                    int NumChoice = int.Parse(Console.ReadLine());
-                    // Add choice to array
-                    // Use that to display modified board
+                    sc.Board(PlayerChoice);
+                    if (i % 2 == 1)
+                    {
+                        // Ensure number is between 1 and 9?
+                        // Error message that won't allow me to add in FirstPlayer
+
+                        Console.WriteLine("Which number on the board would you like to choose {0}?", Player1);
+                        int NumChoice = int.Parse(Console.ReadLine());
+                        // Add choice to array
+                        PlayerChoice[NumChoice] = 'X';
+                        // Use that to display modified board
+                    }
+                    else
+                    {
+                        Console.WriteLine("Which number on the board would you like to choose {0}?", Player2);
+                        int NumChoice = int.Parse(Console.ReadLine());
+                        // Add choice to array
+                        PlayerChoice[NumChoice] = 'O';
+                        // Use that to display modified board
+                    }
+                }
+                Console.WriteLine("Great game!\nWould you like to play another game?(y/n)");
+                char play = char.Parse(Console.ReadLine());
+
+                if (play == 'y')
+                {
+                    keepPlaying = true;
                 }
                 else
                 {
-                    Console.WriteLine("Which number on the board would you like to choose?");
-                    int NumChoice = int.Parse(Console.ReadLine());
-                    // Add choice to array
-                    // Use that to display modified board
+                    keepPlaying = false;
                 }
             }
-
-
         }
     }
 }
+
+
+// maybe use a random number between 1 and 2 to choose which player goes first
+/*int whoGoesFirst = 0;
+Random randNum = new Random();
+whoGoesFirst = randNum.Next(1, 2);
+
+
+if (whoGoesFirst == 1)
+{
+    string FirstPlayer = Player1;
+    string SecondPlayer = Player2;
+}
+else
+{
+    string FirstPlayer = Player2;
+    string SecondPlayer = Player1;
+}*/
